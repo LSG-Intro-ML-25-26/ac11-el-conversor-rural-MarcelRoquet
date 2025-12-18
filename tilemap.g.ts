af57db079410db800c9b0bcf30c8f45f571e3d4e
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2025-12-18 19:03:14 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, main.py, tilemap.g.jres, tilemap.g.ts

---
+++ tilemap.g.ts
@@ -0,0 +1,50 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "tilePath1":
+            case "tile1":return tile1;
+            case "tilePath2":
+            case "tile2":return tile2;
+            case "tilePath4":
+            case "tile3":return tile3;
+            case "tileGrass2":
+            case "tile4":return tile4;
+            case "tilePath7":
+            case "tile5":return tile5;
+            case "tilePath3":
+            case "tile6":return tile6;
+            case "tilePath8":
+            case "tile7":return tile7;
+            case "tilePath5":
+            case "tile8":return tile8;
+            case "tilePath6":
+            case "tile9":return tile9;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
